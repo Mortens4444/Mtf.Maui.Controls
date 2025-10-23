86c2871305a2bb04a415c97b2f8cf94b57c79a46
Mortens <mortens.4444@gmail.com>
2025-10-23 23:00:17 +0200

    Mtf.Maui.Controls NuGet package release 1.0.32

---
+++ Mtf.Maui.Controls/Extensions/ContentPageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace Mtf.Maui.Controls.Extensions;
+
+public static class ContentPageExtensions
+{
+    public static Task<bool> ShowPageAsync(this ContentPage contentPage, Type itemType, Type pageType, object bindingContext)
+    {
+        var genericAsyncMethod = typeof(ContentPageExtensions).GetMethod(nameof(CreatePageWithTypeAsync), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(itemType);
+        return (Task<bool>)genericAsyncMethod.Invoke(null, [contentPage, bindingContext, pageType]);
+    }
+
+    private static async Task<bool> CreatePageWithTypeAsync<T>(ContentPage contentPage, T matchingItem, Type type)
+    {
+        if (!Equals(matchingItem, default(T)))
+        {
+            var pageInstance = Activator.CreateInstance(type, matchingItem);
+            if (pageInstance is Page page)
+            {
+                await contentPage.Navigation.PushAsync(page).ConfigureAwait(true);
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Mtf.Maui.Controls/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Mtf.Maui.Controls.Extensions;
+
+public static class ObservableCollectionExtensions
+{
+    /// <summary>
+    /// Adds the items to the observable collection one by one.
+    /// Simple and reliable; callers are responsible for making the call on the UI thread if needed.
+    /// </summary>
+    public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+        ArgumentNullException.ThrowIfNull(collection);
+        if (items != null)
+        {
+            foreach (var item in items)
+            {
+                collection.Add(item);
+            }
+        }
+    }
+}
