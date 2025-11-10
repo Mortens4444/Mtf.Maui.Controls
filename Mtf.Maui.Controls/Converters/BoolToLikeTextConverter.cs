0d00ef049910856f2d388bd31fd555cb6e911ae7
Mortens <mortens.4444@gmail.com>
2025-11-10 10:03:28 +0100

    Mtf.Maui.Controls NuGet package release 2.0.38

---
+++ Mtf.Maui.Controls/Converters/BoolToLikeTextConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Mtf.Maui.Controls.Converters;
+
+public class BoolToLikeTextConverter : IValueConverter
+{
+    private const string Liked = "";
+    private const string LikeMe = "👍";
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool boolValue)
+        {
+            return String.Empty;
+        }
+
+        return boolValue ? Liked : LikeMe;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
