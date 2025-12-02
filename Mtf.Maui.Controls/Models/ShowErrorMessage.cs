3eebb70891b8189b11796f73c947a8598ed5d623
Mortens <mortens.4444@gmail.com>
2025-12-02 08:57:38 +0100

    Mtf.Maui.Controls NuGet package release 2.0.42

--- Mtf.Maui.Controls/Models/ShowErrorMessage.cs
+++ Mtf.Maui.Controls/Models/ShowErrorMessage.cs
@@ -5,17 +5,23 @@ namespace Mtf.Maui.Controls.Models;
 
 public class ShowErrorMessage : ValueChangedMessage<string>
 {
+    public Exception? Exception { get; }
+
     public ShowErrorMessage(Exception exception)
 #if DEBUG
         : base(exception?.GetDetails() ?? throw new ArgumentNullException(nameof(exception)))
 #else
         : base(exception?.Message ?? throw new ArgumentNullException(nameof(exception)))
 #endif
-    { }
+    {
+        Exception = exception;
+    }
 
     public ShowErrorMessage(string title, Exception exception)
         : base(exception.GetDetails(title))
-    { }
+    {
+        Exception = exception;
+    }
 
     public ShowErrorMessage(string message)
         : base(message)
