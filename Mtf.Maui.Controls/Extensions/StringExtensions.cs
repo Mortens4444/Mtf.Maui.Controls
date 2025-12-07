8c8b60713c2d43decf04e9ef3ef65db9e2ab7196
Mortens <mortens.4444@gmail.com>
2025-12-07 18:49:44 +0100

    Mtf.Maui.Controls NuGet package release 2.0.45

---
+++ Mtf.Maui.Controls/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Mtf.Maui.Controls.Extensions;
+public static partial class StringExtensions
+{
+    public static string ToImageName(this string name)
+    {
+        var imageName = FindCommas().Replace(name, String.Empty);
+        imageName = FindWhiteSpaces().Replace(imageName, "_");
+        return imageName.ToLowerInvariant();
+    }
+
+    [GeneratedRegex("[,]")]
+    private static partial Regex FindCommas();
+
+    [GeneratedRegex("\\s+")]
+    private static partial Regex FindWhiteSpaces();
+}
